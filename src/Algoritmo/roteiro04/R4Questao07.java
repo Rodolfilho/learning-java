d9bcc1466ba98277a57be576098daec54aec4d63
Rodolfo Rocha <rocharodolfo555@gmail.com>
2023-11-13 19:45:02 -0300

    initial commit

--- src/Algoritmo/roteiro04/R4Questao07.java
+++ src/Algoritmo/roteiro04/R4Questao07.java
@@ -1,11 +1,9 @@
 package Algoritmo.roteiro04;
 
-
 import java.util.ArrayList;
 
 public class R4Questao07 {
     public static void main(String[] args) {
-        ArrayList<String> name = new ArrayList<>();
-        System.out.println(name.size());
+
     }
 }
